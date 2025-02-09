f04cf1e44c244d92d4fd88741579e3a93db1baa4
Danny Barajas <dbarajasm93@gmail.com>
2025-02-09 00:27:09 -0800

    use env variables

--- ai_assistant/ai_assistant/settings.py
+++ ai_assistant/ai_assistant/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%9*afnvvr5ze=#5e3i=9g9kxfrm4c=)*l96)1-ib!x=2iae=)6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
